87a35ab3474f3af0cfbfbfa9841ad4588718e97d
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2024-07-16 11:43:31 -0300

    chore: project completed

--- main.js
+++ main.js
@@ -10,6 +10,18 @@ $(document).ready(function () {
 
     $('form').on('submit', function (e) {
         e.preventDefault()
+        const novaImagem = $('#endereco-nova-imagem').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${novaImagem}">`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${novaImagem}">
+                    Ver imagem em tamanho real
+                </a>
+            </div>`).appendTo(novoItem)
+            $(novoItem).appendTo('ul')
+            $(novoItem).fadeIn(1000)
+            $('#endereco-nova-imagem').val(' ')
     })
 
 })
